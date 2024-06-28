65950b806921c0c570558d57c9d0dbb4019bb0d0
vkheroim <vs.kheroim@gmail.com>
2024-06-28 16:49:05 -0300

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,19 @@
+/** @format */
+
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+import {Provider} from "react-redux";
+import {persistor, store} from "./redux/store";
+import {PersistGate} from "redux-persist/integration/react";
+import App from "../src/components/App/App";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
